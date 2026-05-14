536759904b4f408fb7bf6235341f21be2a0879cb
Afia Humayra Shuchi <afiahumayrashuchi@gmail.com>
2026-05-14 22:20:20 +0600

    Initial commit: Sprout environment setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4">
+      <h1 className="text-3xl font-bold text-sprout-primary">
+        Hello Sprout 🌱
+      </h1>
+    </div>
+  )
+}
+
+export default App
